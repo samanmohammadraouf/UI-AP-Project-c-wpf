aa0a4f4cb787ba97b39092fc14bd87c4680d8bd4
samanmohammadraouf <www.saman.raouf@gmail.com>
2020-03-31 15:34:38 +0430

    an final part (adding a new window )
    Signed-off-by: samanmohammadraouf <www.saman.raouf@gmail.com>

---
+++ ap project secon window(final)/ap project secon window/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ap_project_secon_window
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
